51f2fcd6bdbcfae61b56502d31876f746608c32f
Susana0425 <shilpakodithuwakku04@gmail.com>
2025-05-08 15:34:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 /*
-  project_name : 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝙈𝘿 Bot
+  project_name : 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝘽𝙤𝙩 Bot
   Version : 2.4.0
-  Owner : Vibhu Pathum
+  Owner : Hector Manuel
   Do Not Clone 
 
 
 */
 
-//----------------------[ 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝙈𝘿]----------------------//
+//----------------------[ 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝘽𝙤𝙩]----------------------//
 
 const fs = require('fs')
 const { color } = require('./lib/color')
@@ -16,16 +16,16 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID |KANGO~FxIkEY6b#q5-IlW15LQktIwSdTWE2BXsf_E4pWkFlOcVnVsyltac| '' 
-//Enter your 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝙈𝘿 session id here; must start with KANGO~
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~FxIkEY6b#q5-IlW15LQktIwSdTWE2BXsf_E4pWkFlOcVnVsyltac' 
+//Enter your 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝘽𝙤𝙩 session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
 
-global.botname = process.env.BOT_NAME || '𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝙈𝘿' 
+global.botname = process.env.BOT_NAME || '𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝘽𝙤𝙩' 
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+global.ownernumber = process.env.OWNER_NUMBER || '94750255216' 
 
 //-----------------[ OWNER NAME ]------------------//
 
@@ -33,20 +33,20 @@ global.ownername = process.env.OWNER_NAME || 'ᴠɪʙʜᴜ ᴘᴀᴛʜᴜᴍ'
 
 //------------[ STICKER PACKNAME ]-----------------//
 
-global.packname = process.env.STICKER_PACK_NAME || "𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝙈𝘿" 
+global.packname = process.env.STICKER_PACK_NAME || "𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝘽𝙤𝙩" 
 
 //--------------[ STICKER AUTHOR NAME ]------------//
 
-global.author = process.env.STICKER_AUTHOR_NAME || "ᴠɪʙʜᴜᴜ" 
+global.author = process.env.STICKER_AUTHOR_NAME || "ᴍʀ ᴠɪʙʜᴜᴜ" 
 
 //----------------------[ TIMEZONE ]--------------------//
 
-global.timezones = process.env.TIMEZONE || "Asia/Srilanka" 
+global.timezones = process.env.TIMEZONE || "Africa/Accra" 
 //Don't edit this if you don't know!
 
 //----------------[ GITHUB DATABASE ]-----------------//
 
-global.dbToken = process.env.GITHUB_TOKEN || "0WSPUtOewH2V7pObGPrErgAg9moJWs0UUlh9";
+global.dbToken = process.env.GITHUB_TOKEN || "";
 // Not really necessary on panels/vps/termux, just put it when bot settings reset when bot restarts.
 
 //Go to https://github.com/settings/tokens, select 'Tokens (classic)', then tap 'Generate new token' and select 'Generate new token (classic)'. Enter any note, choose 'No expiration', and under 'Select scopes', tick 'repo'. Scroll down, generate the token, and copy it. Paste it here. If using a single token for multiple bots, change the owner number to avoid settings mixups.
@@ -58,13 +58,13 @@ global.plink = process.env.PLINK || "https://youtube.com/@official_manuel"
 
 //------------------[ WATERMARK ]--------------------//
 
-global.wm = process.env.GL_WM || "> ©𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝙈𝘿"
+global.wm = process.env.GL_WM || "> ©𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝘽𝙤𝙩"
 
 //---------------------[ REPLIES ]-----------------------//
 
 global.mess = { 
   done: '*Done*', 
-  success: '> © 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝙈𝘿', 
+  success: '> © 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝘽𝙤𝙩', 
   owner: `*You don't have permission to use this command!*`, 
   group: '*This feature becomes available when you use it in a group!*', 
   admin: '*You’ll unlock this feature with me as an admin!*', 
@@ -86,4 +86,4 @@ fs.watchFile(file, () => {
   require(file)
 })
 
-//----------------------[ 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝙈𝘿 ]----------------------//
+//----------------------[ 𝙈𝙞𝙩𝙕𝙪𝙠𝙮𝙮 𝘽𝙤𝙩 ]----------------------//
